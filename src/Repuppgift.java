c6f2812ec50bbe12525b52dba67a483652106a92
P3teee <te14.peter.berg@umea.nti.se>
2016-09-02 11:30:08 +0200

    repuppgift_1

--- src/Repuppgift.java
+++ src/Repuppgift.java
@@ -4,25 +4,14 @@
 import java.util.Scanner;
 
 public class Repuppgift {
-    public static int[] sortering(int[] numbers) {
 
 
-        for (int i = 0; i < numbers.length; i++) {
-            int index = i;
-            for (int j = i + 1; j < numbers.length; j++)
-                if (numbers[j] > numbers[index])
-                    index = j;
-
-                    int mindrenumber = numbers[index];
-                    numbers[index] = numbers[i];
-                    numbers[i] = mindrenumber;
-                }
-            return numbers;
-        }
-
-        public static void main(String a[]) {
+        public static void main (String a[]){
 
             Scanner tgb = new Scanner(System.in);
+
+
+
             System.out.println("Skriv in ett nummer: ");
             int nummer1 = tgb.nextInt();
             System.out.println("Skriv in ett nummer: ");
@@ -33,8 +22,15 @@ public class Repuppgift {
             int nummer4 = tgb.nextInt();
 
             int[] numbers1 = {nummer1, nummer2, nummer3, nummer4};
-            int[] numbers2 = sortering(numbers1);
-            for(int i:numbers2){
+
+            for(int i = 0; i < numbers1.length / 2; i++)
+            {
+                int temp = numbers1[i];
+                numbers1[i] = numbers1[numbers1.length - i - 1];
+                numbers1[numbers1.length - i - 1] = temp;
+            }
+
+            for (int i : numbers1) {
                 System.out.print(i);
                 System.out.print(", ");
             }
